1f3c7a47124aca822ba7f28c4bccbbf16121bf0c
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2025-04-19 14:25:00 -0300

    feat: add dataCardSale

---
+++ app/_data/data-card-sale.ts
@@ -0,0 +1,26 @@
+interface DataCardSale {
+  title: string;
+  description: string;
+  image: string;
+}
+
+export const dataCardSale: DataCardSale[] = [
+  {
+    title: "Área de membros exclusiva",
+    description:
+      "Ofereça uma experiência única e imersiva para seus clientes com uma área de membros intuitiva e totalmente adaptada às suas necessidades",
+    image: "/images/imagem-vendas-1.webp",
+  },
+  {
+    title: "Vitrine (Marketing place)",
+    description:
+      "Afiliados, explorem nossa vitrine e revendam produtos validados para ganhar comissões incríveis.",
+    image: "/images/imagem-vendas-2.webp",
+  },
+  {
+    title: "Reconhecimento",
+    description:
+      "Nunca foi sorte, sempre foi trabalho duro. Aqui na Winss, te premiamos pelos seus resultados dentro da plataforma. A cada meta conquistada, um prêmio de reconhecimento ao seu trabalho árduo no digital",
+    image: "/images/imagem-vendas-3.webp",
+  },
+];
---
+++ app/_hooks/use-data-card-sale.ts
@@ -0,0 +1,5 @@
+import { dataCardSale } from "../_data/data-card-sale";
+
+export const useDataCardSale = () => {
+  return { dataCardSale };
+};
